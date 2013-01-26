31844b50dfb44ae2142e3cfec3d3b09a5c42d18d
Maciej Żurad <maciej.zurad@gmail.com>
2013-01-26 14:36:00 +0100

    Adding file structure

---
+++ unix-communicator-client/build/unix-communicator-client.build/Debug/unix-communicator-client.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/asiron/Dropbox/Studies/SOP2/SOP2-project/ucc/unix-communicator-client/src/main.c
---
+++ unix-communicator-client/src/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  unix-communicator-client
+//
+//  Created by Maciej Żurad on 1/26/13.
+//  Copyright (c) 2013 Maciej Żurad. All rights reserved.
+//
+
+#include <stdio.h>
+#include <ncurses.h>
+
+
+
+int main(int argc, const char * argv[])
+{
+    initscr();
+    
+    
+    endwin();
+    return 0;
+}
+
